ba12f0bcb938d42252c98efe24d31b3ccc19046a
Dmitry Ilyin <dima@doty.ru>
2020-05-15 22:15:42 +0300

    Translate comments

--- ipc_chip_test.py
+++ ipc_chip_test.py
@@ -11,7 +11,7 @@ def pytest_generate_tests(metafunc):
     def generate_id(input_data, level):
         level += 1
 
-        # Выбираем как это будет выглядеть
+        # Choose how it will look
         INDENTS = {
             # level: (levelmark, addition_indent)
             1: ("_", ["", ""]),
@@ -21,29 +21,29 @@ def pytest_generate_tests(metafunc):
 
         levelmark, additional_indent = INDENTS.get(level, COMMON_INDENT)
 
-        # Если глубже второго уровня - идентификатором становится тип данных
+        # If deeper than 2 level, let's take data type as id
         if level > 3:
             return (
                 additional_indent[0] + type(input_data).__name__ + additional_indent[1]
             )
 
-        # Возвращаем простые данные
+        # Return trivial data types
         elif isinstance(input_data, (str, bool, float, int)):
             return str(input_data)
 
-        # Разбираем список
+        # Parse collection types
         elif isinstance(input_data, (list, set, tuple)):
-            # Погружаемся в список, чтобы проверить те данные, что внутри
+            # Traverse list to check data inside
             list_repr = levelmark.join(
                 [generate_id(input_value, level=level) for input_value in input_data]
             )
             return additional_indent[0] + list_repr + additional_indent[1]
 
-        # Ключи словаря переводим в строку
+        # Convert dictionary keys to string
         elif isinstance(input_data, dict):
             return "{" + levelmark.join(input_data.keys()) + "}"
 
-        # Или ничего для ничего
+        # Or do nothing
         else:
             return None
 
@@ -61,15 +61,15 @@ def pytest_generate_tests(metafunc):
 
     test_cases = []
 
-    # Проходим по нашим сырым данным
+    # Traverse raw data
     for case_id, test_case in enumerate(raw_test_cases):
-        # Ищем список маркеров
+        # Search list of markers
         marks = [getattr(pytest.mark, name) for name in test_case.get("marks", [])]
 
-        # Берем идентификатор из данных, либо сгенерируем
+        # Get specied id or generate it
         case_id = test_case.get("id", generate_id(test_case["hostname"], level=0))
 
-        # Добавляем в наш список сгенерированный из тестовых данных pytest.param
+        # Add test to ready cases
         test_cases.append(pytest.param(test_case, marks=marks, id=case_id,))
 
     return metafunc.parametrize("test_case", test_cases)
